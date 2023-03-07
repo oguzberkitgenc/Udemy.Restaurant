27a7adf62d01207705af8521b24c0bf16dd7db35
Oğuz Berkit Genç <75220168+golgeroot@users.noreply.github.com>
2023-03-07 23:30:11 +0300

    Add project files.

---
+++ Udemy.Restaurant.Entities/Tables/Porsiyon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Udemy.Restaurant.Entities.Tables.Base;
+
+namespace Udemy.Restaurant.Entities.Tables
+{
+    public class Porsiyon :EntityBase
+    {
+        public string Adi { get; set; }
+        public decimal Fiyat { get; set; }
+        public decimal EkMalzemeCarpan { get; set; }
+        public Guid UrunId { get; set; }
+        public virtual Urun Urun { get; set; }
+        public Guid BirimId { get; set; }
+        public virtual Tanim Birim { get; set; }
+    }
+}
---
+++ Udemy.Restaurant.Entities/Tables/Tanim.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Udemy.Restaurant.Entities.Enums;
+using Udemy.Restaurant.Entities.Tables.Base;
+
+namespace Udemy.Restaurant.Entities.Tables
+{
+    public class Tanim : EntityBase
+    {
+        public string Adi { get; set; }
+        public TanimTip TanimTip { get; set; }
+    }
+}
