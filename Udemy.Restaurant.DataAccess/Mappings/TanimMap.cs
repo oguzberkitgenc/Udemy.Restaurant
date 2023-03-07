c945cad33ee3fa377b8de61e3de0ad29eaec5e94
Oğuz Berkit Genç <75220168+golgeroot@users.noreply.github.com>
2023-03-08 00:05:49 +0300

    Mapping Islemleri

---
+++ Udemy.Restaurant.DataAccess/Mappings/TanimMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Udemy.Restaurant.Entities.Tables;
+
+namespace Udemy.Restaurant.DataAccess.Mappings
+{
+    public class TanimMap : EntityTypeConfiguration<Tanim>
+    {
+        public TanimMap()
+        {
+            Property(c=>c.Adi).HasMaxLength(50);
+            ToTable("Tanimlar");
+            Property(c => c.Adi).HasColumnName("Adi");
+            Property(c => c.TanimTip).HasColumnName("TanimTip");
+        }
+
+    }
+}
---
+++ Udemy.Restaurant.DataAccess/Mappings/UrunMap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Udemy.Restaurant.Entities.Tables;
+
+namespace Udemy.Restaurant.DataAccess.Mappings
+{
+    public class UrunMap : EntityTypeConfiguration<Urun>
+    {
+        public UrunMap()
+        {
+            Property(c=>c.Adı).HasMaxLength(50);
+            Property(c=>c.Barkod).HasMaxLength(50);
+
+            ToTable("Urunler");
+            Property(c => c.Adı).HasColumnName("Adi");
+            Property(c => c.Barkod).HasColumnName("Barkod");
+            Property(c => c.Fotograf).HasColumnName("Fotograf");
+            Property(c => c.UrunGrupId).HasColumnName("UrunGrupId");
+        }
+    }
+}
