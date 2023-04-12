211583ef9b238b625d925e4c3ebc26c76d5293bd
Oğuz Berkit Genç <75220168+golgeroot@users.noreply.github.com>
2023-04-13 00:43:47 +0300

    ..

---
+++ Udemy.Restaurant.DataAccess/Dals/TelefonDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Udemy.Restaurant.DataAccess.Interfaces;
+using Udemy.Restaurant.Entities.Tables;
+
+namespace Udemy.Restaurant.DataAccess.Dals
+{
+	public class TelefonDal : Repository<Telefon>, ITelefonDal
+	{
+		public TelefonDal(DbContext context) : base(context)
+		{
+
+		}
+	}
+}
