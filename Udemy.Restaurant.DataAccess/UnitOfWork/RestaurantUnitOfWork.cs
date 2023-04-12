18ebc0f14629343661d821ad131460c86822340b
Oğuz Berkit Genç <75220168+golgeroot@users.noreply.github.com>
2023-04-13 00:56:41 +0300

    ..

--- Udemy.Restaurant.DataAccess/UnitOfWork/RestaurantUnitOfWork.cs
+++ Udemy.Restaurant.DataAccess/UnitOfWork/RestaurantUnitOfWork.cs
@@ -21,7 +21,9 @@ namespace Udemy.Restaurant.DataAccess.UnitOfWork
         public ITanimDal TanimDal { get; set; }
         public IPorsiyonDal PorsiyonDal { get; set; }
         public IEkMalzemeDal EkMalzemeDal { get; set; }
-
+        public IMusteriDal MusteriDal { get; set; }
+        public ITelefonDal TelefonDal { get; set; }
+        public IAdresDal AdresDal { get; set; }
         public RestaurantUnitOfWork(string connectionString=null)
         {
             if (connectionString==null)
@@ -36,6 +38,9 @@ namespace Udemy.Restaurant.DataAccess.UnitOfWork
             TanimDal = new TanimDal(_context);
             PorsiyonDal = new PorsiyonDal(_context);
             EkMalzemeDal = new EkMalzemeDal(_context);
+            MusteriDal= new MusteriDal(_context);
+            TelefonDal= new TelefonDal(_context);
+            AdresDal= new AdresDal(_context);
         }
         public bool Commit() // Kayıt
         {
